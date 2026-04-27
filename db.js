7fa3662d9e71b95d5c557e09dba7b9c00819db90
korot0 <108902180+korot0@users.noreply.github.com>
2026-04-27 12:49:11 -0500

    Set up server and add import/export statements to routes

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Database = require("better-sqlite3");
 const db = new Database("library.db");
 
+module.exports = db;
+
 // Testing db
-const query = db.prepare("select * from borrower where name = ?");
-const row = query.get("Alex Kim");
-console.log(row);
+// const query = db.prepare("select * from borrower where name = ?");
+// const row = query.get("Alex Kim");
