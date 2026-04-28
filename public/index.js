d8ec6bd9ccc800773aa1a380e605edc00f81682c
Josue Trejo <jt@jt-2.local>
2026-04-27 21:13:15 -0500

    Implement borrower, book, and copies routes

--- public/index.js
+++ public/index.js
@@ -13,8 +13,39 @@ buttons.forEach((btn) => {
   });
 });
 
-const bookTitleInput = document.querySelector("#copies-title");
+// 2) Add information about a new Borrower. Do not provide the CardNo in your query. Output the card number as if you are giving a new library card.
+const addBorrower = async () => {};
 
-const copiesPerBranch = () => {
-  console.log(bookTitleInput.value);
+// 3) Add a new Book with publisher (use can use a publisher that already exists) and author information to all 5 branches with 5 copies for each branch. Submit your editable SQL query that your code executes.
+const addBook = async () => {
+  const title = document.querySelector("#book-title").value;
+  const authorName = document.querySelector("#book-author").value;
+  const publisher = document.querySelector("#book-publisher").value;
+
+  const res = await fetch(`/api/book`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      authorName,
+      publisher,
+    }),
+  });
+
+  // TODO: Display data
+  const data = await res.json();
+  console.log(data);
+};
+
+// 4) Given a book title list the number of copies loaned out per branch.
+const copiesPerBranch = async () => {
+  const title = document.querySelector("#copies-title").value;
+
+  const res = await fetch(`/api/copies?title=${title}`);
+
+  // TODO: Display data
+  const data = await res.json();
+  console.log(data);
 };
--- routes/book.js
+++ routes/book.js
@@ -1,24 +1,26 @@
+// 3) Add a new Book with publisher (use can use a publisher that already exists) and author information to all 5 branches with 5 copies for each branch.
+
 const db = require("../db.js");
 const express = require("express");
 
 const bookRouter = express.Router();
 
 bookRouter.post("/book", (req, res) => {
-  const { title, publisher, authorName } = req.body;
+  const { title, authorName, publisher } = req.body;
 
   const insertBook = db.prepare(
-    `insert into book (title, publisher_name) values (?, ?)`,
+    `insert into book (title, publisher_name) values (?, ?);`,
   );
 
   const insertAuthor = db.prepare(
-    `insert into book_authors (book_id, author_name) values(?, ?)`,
+    `insert into book_authors (book_id, author_name) values(?, ?);`,
   );
 
   const insertCopies = db.prepare(
-    `insert into book_copies(book_id, branch_id, no_of_copies) values (?, ?, ?)`,
+    `insert into book_copies(book_id, branch_id, no_of_copies) values (?, ?, ?);`,
   );
 
-  const getBranches = db.prepare(`select branch_id from library_branch`);
+  const getBranches = db.prepare(`select branch_id from library_branch;`);
 
   const transaction = db.transaction(() => {
     const bookResult = insertBook.run(title, publisher);
