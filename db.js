827b51be11e322e1b3df31124d9f294189efb46a
ctran42 <christophertran1004@gmail.com>
2026-04-27 21:43:43 -0500

    Added trigger for update on Book_Loans

--- db.js
+++ db.js
@@ -1,4 +1,17 @@
 const Database = require("better-sqlite3");
 const db = new Database("library.db");
 
+db.pragma("foreign_keys = ON");
+
+db.exec(`
+CREATE TRIGGER IF NOT EXISTS checkout_book_copy
+AFTER INSERT ON BOOK_LOANS
+BEGIN
+		UPDATE BOOK_COPIES
+		SET no_of_copies = no_of_copies - 1
+		WHERE book_id = NEW.book_id
+			AND branch_id = NEW.branch_id;
+END;
+`);
+
 module.exports = db;
